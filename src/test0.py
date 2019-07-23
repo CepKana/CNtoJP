59ae76e823debf11d91bab9e0677234db06426b9
CepKana <2244305500@qq.com>
2019-07-23 19:53:01 +0800

    测试了txt文件的转换，成功。

--- src/test0.py
+++ src/test0.py
@@ -1,5 +1,16 @@
 # -*- coding: utf-8 -*-
-import chardet
+#import chardet
+import os
 
-s = '我的师姐'.encode('gbk')
-print(chardet.detect(s))
+#s = '我的师姐'.encode('gbk')
+#print(chardet.detect(s))
+
+
+fd = open('test.txt', 'r', encoding='utf-8')
+fd0 = fd.read().encode('gbk').decode('Shift-JIS')
+fd.close()
+print(fd0)
+fd1 = open('test.txt', 'w+', encoding='utf-8')
+fd1.write(fd0)
+fd1.close()
+
